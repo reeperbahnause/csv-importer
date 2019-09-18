d6ba73d6f5c82ce46354479970a10be593ac7a16
James Cole <thegrumpydictator@gmail.com>
2019-09-18 06:39:19 +0200

    Update copyright statements.

--- config/logging.php
+++ config/logging.php
@@ -1,28 +1,26 @@
 <?php
-declare(strict_types=1);
-
-
 /**
  * logging.php
  * Copyright (c) 2019 thegrumpydictator@gmail.com
  *
- * This file is part of Firefly III CSV Importer
+ * This file is part of Firefly III CSV Importer.
  *
- * Firefly III CSV Importer is free software: you can redistribute it
- * and/or modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
+ * Firefly III CSV Importer is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Firefly III CSV Importer is distributed in the hope that it will
- * be useful, but WITHOUT ANY WARRANTY; without even the implied
- * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
+ * Firefly III CSV Importer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Firefly III CSV Importer. If not, see
+ * along with Firefly III CSV Importer.If not, see
  * <http://www.gnu.org/licenses/>.
  */
 
+declare(strict_types=1);
 
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
--- config/twigbridge.php
+++ config/twigbridge.php
@@ -1,12 +1,24 @@
 <?php
 
 /**
- * This file is part of the TwigBridge package.
+ * twigbridge.php
+ * Copyright (c) 2019 thegrumpydictator@gmail.com
  *
- * @copyright Robert Crowe <hello@vivalacrowe.com>
+ * This file is part of Firefly III CSV Importer.
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * Firefly III CSV Importer is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Firefly III CSV Importer is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Firefly III CSV Importer.If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 
 /**
