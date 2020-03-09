ddbdbac701615104e27794ee81af3e9dc3b60fc6
James Cole <thegrumpydictator@gmail.com>
2020-03-09 17:56:17 +0100

    Update version.

--- config/csv_importer.php
+++ config/csv_importer.php
@@ -38,11 +38,11 @@ use App\Services\Import\Task\PositiveAmount;
 use App\Services\Import\Task\Tags;
 
 return [
-    'version'         => '1.0.0-beta.1',
+    'version'         => '1.0.0-beta.2',
     'access_token'    => env('FIREFLY_III_ACCESS_TOKEN'),
     'uri'             => env('FIREFLY_III_URI'),
     'upload_path'     => storage_path('uploads'),
-    'minimum_version' => '5.0.0-beta.1',
+    'minimum_version' => '5.1.1',
     'cache_api_calls' => false,
     'delimiters'      => [
         'comma'     => ',',
