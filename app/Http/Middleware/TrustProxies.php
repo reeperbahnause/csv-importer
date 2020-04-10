54054fc1f532e3914d17868886643644c69f7afc
James Cole <thegrumpydictator@gmail.com>
2020-04-10 20:37:25 +0200

    Respect trusted proxies. https://github.com/firefly-iii/firefly-iii/issues/3252

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -26,6 +26,7 @@ declare(strict_types=1);
 namespace App\Http\Middleware;
 
 use Fideloper\Proxy\TrustProxies as Middleware;
+use Illuminate\Contracts\Config\Repository;
 use Illuminate\Http\Request;
 
 /**
@@ -46,4 +47,19 @@ class TrustProxies extends Middleware
      * @var int
      */
     protected $headers = Request::HEADER_X_FORWARDED_ALL;
+
+    /**
+     * TrustProxies constructor.
+     *
+     * @param Repository $config
+     */
+    public function __construct(Repository $config)
+    {
+        $trustedProxies = (string) config('csv_importer.trusted_proxies');
+        $this->proxies  = explode(',', $trustedProxies);
+        if ('**' === $trustedProxies) {
+            $this->proxies = '**';
+        }
+        parent::__construct($config);
+    }
 }
