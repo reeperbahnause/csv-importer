2d74e07d393f29126ac5cc7c7e6c63bbdc10e70e
James Cole <james@firefly-iii.org>
2020-05-16 06:29:23 +0200

    Fix path.

--- app/Http/Controllers/Import/StartController.php
+++ app/Http/Controllers/Import/StartController.php
@@ -56,8 +56,13 @@ class StartController extends Controller
         $subTitle  = 'Start page and instructions';
 
         // get existing configs.
-        Log::debug(sprintf('Going to check directory for config files: %s', config('filesystems.configurations.root')));
         $disk = Storage::disk('configurations');
+        Log::debug(
+            sprintf(
+                'Going to check directory for config files: %s',
+                config('filesystems.disks.configurations.root'),
+            )
+        );
         $list = $disk->files();
 
         Log::debug('List of files:', $list);
